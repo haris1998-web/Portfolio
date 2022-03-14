00d97f69cb5b98effe7941c0291a1c4642e700de
Muhammad Haris Yousaf <muhammadharisyousaf@gmail.com>
2022-03-14 10:26:24 +0500

    Initial Commit

---
+++ src/data.js
@@ -0,0 +1,24 @@
+export const projects = [
+    {
+        title: "Final Year Project",
+        subtitle: "Django and React-Redux",
+        description: "Worked as a team member and built the backend using Django and Django Rest Framework",
+        image: "https://drive.google.com/uc?export=view&id=1JPFJZcxLXNFHPK-J1HBkjFf7aIJYjfH9",
+        link: "",
+    }
+]
+
+export const skills = [
+    "Object Oriented Programming",
+    "Databases",
+    "Data Analysis",
+    "Data Visualization",
+    "Python",
+    "Django",
+    "Django Rest Framework",
+    "ReactJS",
+    "Redux",
+    "Pandas",
+    "Tensorflow",
+    "Keras",
+]
